553ab292da113cf373acc0253c0e1b637a800237
muha.io <zhanmuha01@gmail.com>
2022-12-14 18:03:22 +0600

    remove keras

--- app.py
+++ app.py
@@ -61,31 +61,6 @@ def predict_gradient():
     return {
         'prediction': pd.Series(prediction).to_json(orient='values'),
     }
-"""
-
-@app.route('/predict/keras', methods=['POST'])
-def predict():
-    if not request.json:
-        abort(400)
-    data = request.json
-    sample = {
-        "salary(thousand kzt)": data['salary'],
-        "n_of_individual_projects": data['projects'],
-        "motivation_r": data['motivation'] / 100,
-        "relationship_with_others_r": data['relationship'] / 100,
-        "communication_skills_r": data['communication'] / 100,
-        "task_management_r": data['task_management'] / 100,
-        "total_rating": data['total_rating'] / 100,
-    }
-    model = load_model('models/keras.h5')
-    df = pd.DataFrame(sample, index=[0])
-    input_dict = {name: df[name].astype(float) for name in df.columns}
-    predictions = model.predict(input_dict)
-    return {
-        'prediction': int(predictions[0][0] * 100),
-        'formData': data,
-    }
- """
 
 if __name__ == '__main__':
     app.run(debug=True)
